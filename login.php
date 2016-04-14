aec7208103e619df67d166319e737c3068eeda04
Laura Delaney <lauracatspam2@gmail.com>
2016-04-13 21:14:54 -0700

    Update login.php

--- login.php
+++ login.php
@@ -19,35 +19,11 @@
 	echo "The current time is $arrivaldate[hours]:$arrivaldate[minutes]:$arrivaldate[seconds], $arrivaldate[month], $arrivaldate[mday], $arrivaldate[year]";
 	echo "<br>";
 	
-	//send the username, password, and timestamp to the database
-	//Server: syllabusscheduler.database.windows.net,1433 
-	//SQL Database: Syllabus Database\r\nUser Name: HowToMakeDarn
-	
-	//PHP Data Objects(PDO) Sample Code:
-	
-	try {
-		$conn = new PDO ( \"sqlsrv:server = tcp:syllabusscheduler.database.windows.net,1433; Database = Syllabus Database\", \"HowToMakeDarn\", \"{-NearAnything1@}\");
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		}
-		catch ( PDOException $e ) {
-		print( \"Error connecting to SQL Server.\" );
-		die(print_r($e));
-		}
-		//SQL Server Extension Sample Code:
-		$connectionInfo = array(\"UID\" => \"HowToMakeDarn@syllabusscheduler\", \"pwd\" => \"{-NearAnything1@}\", \"Database\" => \"Syllabus Database\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);
-		$serverName = \"tcp:syllabusscheduler.database.windows.net,1433\";
-		$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
 	
-
-/*
+	
 	//w3schools example of how to connect
-$servername = "syllabusscheduler.database.windows.net,1433";
+$servername = "tcp:syllabusscheduler.database.windows.net,1433";
 $username = "HowToMakeDarn";
 $password = "-NearAnything1@";
 
@@ -56,12 +32,11 @@ $conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
-    //die("Connection failed: " . mysqli_connect_error());
-    echo "was unable to";
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 echo "Connected successfully";	
-*/
+
 	
 ?>
 </body>
