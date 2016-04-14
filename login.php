40866dc30fc2a32d23b182cc3909eefe011baf10
Laura Delaney <lauracatspam2@gmail.com>
2016-04-13 21:16:43 -0700

    Update login.php

--- login.php
+++ login.php
@@ -28,14 +28,17 @@ $username = "HowToMakeDarn";
 $password = "-NearAnything1@";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";	
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 
 	
 ?>
