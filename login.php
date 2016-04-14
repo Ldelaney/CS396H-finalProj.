ccf782b140f0fdfbc93b4cf844b614d972ecfd1b
Laura Delaney <lauracatspam2@gmail.com>
2016-04-13 21:08:41 -0700

    Update login.php

--- login.php
+++ login.php
@@ -50,13 +50,13 @@ $username = "HowToMakeDarn";
 $password = "-NearAnything1@";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+//$conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
-if (!$conn) {
-    //die("Connection failed: " . mysqli_connect_error());
-    echo "was unable to";
-}
+//if (!$conn) {
+//    //die("Connection failed: " . mysqli_connect_error());
+//    echo "was unable to";
+//}
 
 echo "Connected successfully";	
 	
