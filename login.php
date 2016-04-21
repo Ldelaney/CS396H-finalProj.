44b7559c82992ba0f4022686a015b62d0446d54f
Laura Delaney <lauracatspam2@gmail.com>
2016-04-20 19:02:17 -0700

    Update login.php

--- login.php
+++ login.php
@@ -19,7 +19,6 @@
 	echo "The current time is $arrivaldate[hours]:$arrivaldate[minutes]:$arrivaldate[seconds], $arrivaldate[month], $arrivaldate[mday], $arrivaldate[year]";
 	echo "<br>";
 	
-
 	
 	//connect to the database
 try {
@@ -31,11 +30,20 @@ catch ( PDOException $e ) {
     	die(print_r($e));
 	}
 	
+	//Prepare an SQL instruction to be executed, in this case, an insertion
+	$stmt = $conn->prepare("INSERT INTO Users (username, password, userArrivalTimeInt) VALUES (?, ?, ?)");
+	//bind parameters to the statement (this way of doing things protects better against SQL injection attacks than building a string)
+	$stmt->bind_param("ssi", $userusername, $userpassword, $arrivalTime);
+	$stmt->execute();
+	echo "New record created successfully";
+	/*
 	//code right below this modified from w3schools (http://www.w3schools.com/php/php_mysql_insert.asp)
-	$sql = "INSERT INTO Users (username, password, userArrivalTime) VALUES ($userusername, $userpassword, $arrivalTime)"; //FIXME: arrivaltime may have a format mismatch -- seconds since UNIX epoc vs mySQL datetime
+	$sql = "INSERT INTO Users (username, password, userArrivalTime) 
+	VALUES ($userusername, $userpassword, $arrivalTime)"; //FIXME: arrivaltime may have a format mismatch -- seconds since UNIX epoc vs mySQL datetime
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
+    */
 	
 ?>
 </body>
