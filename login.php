07fc67f5b019fe6550f436010e056cd8f29ac0c9
Laura Delaney <lauracatspam2@gmail.com>
2016-04-20 18:58:49 -0700

    Update login.php
    use a prepared statement instead of building a string of SQL.

--- login.php
+++ login.php
@@ -5,8 +5,8 @@
 	//set the variables
  	$userusername =  $_POST["name"];
  	$userpassword = $_POST["userpassword"];
-	$arrivaltime = time();
-	$arrivaldate = getdate($arrivaltime);
+	$arrivalTime = time();
+	$arrivaldate = getdate($arrivalTime);
 	
 	//echo the variables, confirm they were passed correctly
 	ini_set('display_errors', 1);
@@ -19,9 +19,8 @@
 	echo "The current time is $arrivaldate[hours]:$arrivaldate[minutes]:$arrivaldate[seconds], $arrivaldate[month], $arrivaldate[mday], $arrivaldate[year]";
 	echo "<br>";
 	
-
 	
-	//connect to the databse
+	//connect to the database
 try {
    $conn = new PDO ( "sqlsrv:server = tcp:syllabusscheduler.database.windows.net,1433; Database = Syllabus Database", "HowToMakeDarn", "-NearAnything1@");
        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -30,6 +29,22 @@ catch ( PDOException $e ) {
    		print( "Error connecting to SQL Server." );
     	die(print_r($e));
 	}
+	
+	//Prepare an SQL instruction to be executed, in this case, an insertion
+	$stmt = $conn->prepare("INSERT INTO Users (username, password, userArrivalTimeInt) VALUES (?, ?, ?)");
+	//bind parameters to the statement (this way of doing things protects better against SQL injection attacks than building a string)
+	$stmt->bind_param("ssi", $userusername, $userpassword, $arrivalTime);
+	$stmt->execute();
+	echo "New record created successfully";
+	/*
+	//code right below this modified from w3schools (http://www.w3schools.com/php/php_mysql_insert.asp)
+	$sql = "INSERT INTO Users (username, password, userArrivalTime) 
+	VALUES ($userusername, $userpassword, $arrivalTime)"; //FIXME: arrivaltime may have a format mismatch -- seconds since UNIX epoc vs mySQL datetime
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    */
+	
 ?>
 </body>
 </html>
