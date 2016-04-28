c5b079ae268edd45197ae56cf4f706678045487f
Laura Delaney <lauracatspam2@gmail.com>
2016-04-27 19:24:16 -0700

    Create results.php

---
+++ results.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+  //Display results
+  echo "Results: <br>";
+  //FIXME: remove this for launch
+  echo "user is " . $_SESSION["user"] . "<br>";
+  echo "password is " . $_SESSION["password"]. "<br>";
+  //end of things to be removed
+?>
