203b32127832c5191628ae64eea781169f1b7bd6
Laura Delaney <lauracatspam2@gmail.com>
2016-04-27 19:32:10 -0700

    Update results.php

--- results.php
+++ results.php
@@ -5,8 +5,8 @@
   //Display results
   echo "Results: <br>";
   //FIXME: remove this for launch
-  //echo "user is " . $_SESSION["user"] . "<br>";
-  //echo "password is " . $_SESSION["password"]. "<br>";
-  print_r($_SESSION);
+  echo "user is " . $_SESSION["user"] . "<br>";
+  echo "password is " . $_SESSION["password"]. "<br>";
+  //print_r($_SESSION);
   //end of things to be removed
 ?>
