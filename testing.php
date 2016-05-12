e0a742954c6ae0d5ad328e6f1d33d160d110db24
Laura Delaney <lauracatspam2@gmail.com>
2016-05-11 19:38:52 -0700

    Update testing.php
    continue expanding the bughunt range

--- testing.php
+++ testing.php
@@ -23,18 +23,18 @@
   
   use PhpOffice\PhpWord\Autoloader;
   use PhpOffice\PhpWord\Settings;
-  /*
+  
   error_reporting(E_ALL);
-define('CLI', (PHP_SAPI == 'cli') ? true : false);
-define('EOL', CLI ? PHP_EOL : '<br />');
-define('SCRIPT_FILENAME', basename($_SERVER['SCRIPT_FILENAME'], '.php'));
-define('IS_INDEX', SCRIPT_FILENAME == 'index');
-Autoloader::register();
-Settings::loadConfig();
+  define('CLI', (PHP_SAPI == 'cli') ? true : false);
+  define('EOL', CLI ? PHP_EOL : '<br />');
+  define('SCRIPT_FILENAME', basename($_SERVER['SCRIPT_FILENAME'], '.php'));
+  define('IS_INDEX', SCRIPT_FILENAME == 'index');
+  Autoloader::register();
+  Settings::loadConfig();
   
   //aren't using writers
   //this is the end of the part that would be a header file
-  
+  /*
   //set the name of where we are, and the name of the source file
   $name = basename(__FILE__, '.php');
   $source = __DIR__ . '/uploads/HW5.docx';
