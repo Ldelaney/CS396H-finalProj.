2917dfdea0a81cb0ed7c0eff6ae2c27c21e5ceb8
Laura Delaney <lauracatspam2@gmail.com>
2016-05-11 19:25:31 -0700

    Update testing.php

--- testing.php
+++ testing.php
@@ -9,18 +9,28 @@
 <body>
 
 <?php
+	
   $outputfile = fopen("testfile.txt", "w");
   $msg = "This is a line of test output";
   fwrite($outputfile, $msg);
   fclose($outputfile);
   
+  echo __DIR__;
+  
+  /*
+  require_once __DIR__ . '/../src/PhpWord/Autoloader.php';
+  use PhpOffice\PhpWord\Autoloader;
+  use PhpOffice\PhpWord\Settings;
+  
   //set the name of where we are, and the name of the source file
   $name = basename(__FILE__, '.php');
-  $source = __DIR__ . "/uploads/HW5.docx";
-  echo "now reading contents from " . $source. "<br>";
+  $source = __DIR__ . '/uploads/HW5.docx';
+  echo "now reading contents from " . $source;
   $phpWord = \PhpOffice\PhpWord\IOFactory:::load($source);
 	//this should read things?
 	//the library has OK documentation and examples, but I am still confused
+*/
+
 ?>
 </body>
 </html>
