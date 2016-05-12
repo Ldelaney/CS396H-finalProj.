f52b36501a27c2e3dd6edc972bce0e3b28eeda68
Laura Delaney <lauracatspam2@gmail.com>
2016-05-11 20:29:13 -0700

    Update testing.php

--- testing.php
+++ testing.php
@@ -47,7 +47,7 @@
   //this is the end of the part that would be a header file
   
   //set the name of where we are, and the name of the source file
-  $name = basename(__FILE__, '.php');
+  $name = basename(__FILE__ .  '.php');
   $source = __DIR__ . '/uploads/HW5.docx';
  echo $name . '<br>';
  echo 'now reading contents from ' . $source;
